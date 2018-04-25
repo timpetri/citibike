61f48fa2e2fc95546d8061d2fbe43a3baddc9d64
Tom Meagher <thom.meagher@gmail.com>
2018-04-24 23:19:56 -0400

    Add gitignore, requirements.txt

--- bikescraper.py
+++ bikescraper.py
@@ -3,6 +3,7 @@ import time
 import json
 import random
 import os
+import time
 
 feed_endpoint = 'https://layer.bicyclesharing.net/map/v1/nyc/stations'
 
@@ -10,17 +11,24 @@ feed_endpoint = 'https://layer.bicyclesharing.net/map/v1/nyc/stations'
 num_iterations = 1
 
 def run():
-	grab_and_save()
-
+	counter = 0
+	while True:
+		counter += 1
+		grab_and_save(counter)
+		time.sleep(30)
 
 def create_file_name():
-	return time.strftime("%Y%m%d-%H%M%S")
+	return time.strftime("%Y-%m-%d-%H-%M-%S")
 
 
-def grab_and_save():
-	reponse = requests.get(feed_endpoint)
-	data = r.json()
-	file_name = f'./data/{create_file_name()}-citidata.json' 
+def grab_and_save(counter):
+	response = requests.get(feed_endpoint)
+	data = response.json()
+	file_name = f'./data/{create_file_name()}.json' 
+	print(f"Iteration #{counter}")
+	print(f"Status code: {response.status_code}")
+	print(f"File name: {file_name}")
+	print("===================================")
 	with open(file_name, 'w') as f:
 		json.dump(data, f)
 
